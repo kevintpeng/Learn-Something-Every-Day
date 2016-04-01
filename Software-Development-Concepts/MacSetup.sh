7dd0a83393e26a8c40456985ab8541d73bf91a72
Kevin Peng <kpeng97@gmail.com>
2016-03-31 22:35:14 -0400

    Update MacSetup.sh

--- Software-Development-Concepts/MacSetup.sh
+++ Software-Development-Concepts/MacSetup.sh
@@ -1,3 +1,18 @@
+echo SCRIPT: Getting fonts
+mkdir Code
+cd Code
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+
+echo SCRIPT: Installing Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 echo SCRIPT: Starting HomeBrew install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
 
+echo SCRIPT: Downloading Node and npm
+export PATH="/usr/local/bin:$PATH"  # temporarily adds path var
+brew install node
+npm install -g grunt-cli   # grunt install, and test
