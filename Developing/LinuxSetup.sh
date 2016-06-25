a311546be7122d7601aab71772fe381076c65f06
Kevin Peng <kpeng97@gmail.com>
2016-06-25 13:13:47 -0400

    Update LinuxSetup.sh

--- Developing/LinuxSetup.sh
+++ Developing/LinuxSetup.sh
@@ -1,9 +1,17 @@
+# apt stuff + ngnix
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
+
+# ruby-build dependencies
 sudo apt-get install g++-4.9
 sudo apt-get remove make
 sudo apt-get install make
 sudo apt-get install gcc
+sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+
+sudo apt-get install nginx
+
+# rbenv / ruby
 cd
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
