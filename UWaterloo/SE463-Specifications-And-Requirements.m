eb43357e7495731a6f1fa808383b0432cb908936
Kevin Peng <kpeng97@gmail.com>
2019-07-19 11:23:20 -0400

    Create SE463-Specifications-And-Requirements.m

---
+++ UWaterloo/SE463-Specifications-And-Requirements.m
@@ -0,0 +1,21 @@
+# [SE 463 Specifications and Requirements](https://github.com/m-socha/SE-463-Notes/blob/master/notes.pdf)
+
+### 2 Business Model
+- **waterfall** relies on meeting customer needs with initial implementation, and leads to requirements-related problems
+- **lean product development model** helps find problem fit and market fit
+- **lean canvas** is a template to help determine the initial reqs of a project
+
+### 3 Hypothesis Testing
+- aim to validate/invalidate assumptions
+- **falsifiable** hypotheses aim to be easily proven wrong (forall instead of exists)
+- **customer risk**: is there a viable customer segment?
+- **product risk**: solving the wrong problems, how do customers rank the outlined problems
+- **market risk**: not being better than the competition
+- **problem interview** is used to test hypotheses, to explore customer's worldview
+
+### 4 Stakeholders
+- **onwer/client** is paying for development. **champion** stakeholder
+- **customers** buy the product
+- **user** operate the software
+
+### 5 User Requirements
